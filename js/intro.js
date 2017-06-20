b46db2ff27393e5f4c36915bc59dbc90fd39d0be
Diana Shkolnikov <dianashk@gmail.com>
2017-06-19 20:14:17 -0400

    simplify intro.js to send authenticate message to main process

--- js/intro.js
+++ js/intro.js
@@ -1,23 +1,5 @@
-const { ipcRenderer, shell } = require('electron');
-const { dialog } = require('electron').remote;
-const settings = require('electron-settings');
-const config = require('../config.json');
-const { authenticate } = require('../mapzen-util/src/js/authRenderer');
+const { ipcRenderer } = require('electron');
 
-document.getElementById('body').onload = _ => {
-  // this will require the user to login before loading anything else
-  authenticate(
-    config.auth,
-    ipcRenderer,
-    settings.get('clearAuthCache') || false,
-    function (err) {
-      console.log('authentication is done');
-
-      if (err) {
-        process.exit();
-      }
-
-      ipcRenderer.send('loadPage', 'apiKey');
-    }
-  );
-};
+function onLogin() {
+  ipcRenderer.send('authenticate');
+}
